82af54516db7030c7fa7459874fd767ab5aeadf6
Brian Simiyu <briansimiyuj@gmail.com>
2023-10-02 09:42:12 +0300

    Box functionality

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,11 +1,23 @@
-const Box = () =>{
+const Box = ({ colorValue }) =>{
 
     return(
 
-        <div className="box"></div>
+        <div className="box" style={{backgroundColor: colorValue}}>
+
+            <p>{colorValue ? colorValue : 'Empty Value'}</p>
+
+        </div>
 
     )
 
 }
+
+
+Box.defaultProps ={
+
+    colorValue: 'empty color value',
+
+}
+
 
 export default Box
